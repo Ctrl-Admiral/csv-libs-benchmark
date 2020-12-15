9b2a51f7e442ccf2b42444792149e3994cd29917
Ctrl-Admiral <sigavarina@edu.hse.ru>
2020-12-15 22:05:21 +0300

    Add benchmark of aria's library

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <rapidcsv.h>
+#include <parser.hpp>
 #include <benchmark/benchmark.h>
 
 #include <iostream>
 #include <fstream>
+
+namespace acsv = aria::csv;
 
 static void BM_RAPID_SERIALIZATION(benchmark::State& state)
 {
@@ -39,10 +42,55 @@ static void BM_RAPID_DESERIALIZATION(benchmark::State& state)
         }
         state.ResumeTiming();
     }
+}
 
+static void BM_CSV_PARSER_SERIALIZATION(benchmark::State& state)
+{
+    std::string filename = "../../csv/file" + std::to_string(state.range(0)) + ".csv";
+    std::ifstream file(filename);
+    acsv::CsvParser doc(file);
+
+    for (auto _ : state)
+    {
+        {
+            state.PauseTiming();
+            std::stringstream parsed_csv;
+            state.ResumeTiming();
+            for (auto& row : doc)
+            {
+              for (auto& field : row)
+              {
+                parsed_csv << field << ',';
+              }
+              parsed_csv << '\n';   // to make output like output of other libs
+            }
+            state.PauseTiming();  // before killing stream
+        }
+        state.ResumeTiming();
+    }
+}
+
+static void BM_CSV_PARSER_DESERIALIZATION(benchmark::State& state)
+{
+    std::string filename = "../../csv/file" + std::to_string(state.range(0)) + ".csv";
+    std::ifstream file(filename);
+    std::string str((std::istreambuf_iterator<char>(file)),
+                     std::istreambuf_iterator<char>());
+    std::istringstream ss(str);
+    for (auto _ : state)
+    {
+        {
+            acsv::CsvParser doc{ss};
+            benchmark::DoNotOptimize(doc);
+            state.PauseTiming();
+        }
+        state.ResumeTiming();
+    }
 }
 
 BENCHMARK(BM_RAPID_SERIALIZATION)->DenseRange(0,9,1);
 BENCHMARK(BM_RAPID_DESERIALIZATION)->DenseRange(0,9,1);
+BENCHMARK(BM_CSV_PARSER_SERIALIZATION)->DenseRange(0,9,1);
+BENCHMARK(BM_CSV_PARSER_DESERIALIZATION)->DenseRange(0,9,1);
 
 BENCHMARK_MAIN();
